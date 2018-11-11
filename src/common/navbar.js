9315550437fea3a50a73b3d1f33a7d929f163ff5
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-11 07:52:51 -0800

    Added basic offcanvas navigation.

--- src/common/navbar.js
+++ src/common/navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { FiAlignLeft } from 'react-icons/fi';
+import './styles/navbar.css'
 
 
 class NavBar extends React.Component {
@@ -19,26 +20,39 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="w-100 bg-near-white">
         <div
-          className="w-100 pa2 flex justify-between items-center">
-          <div className="w1">
-            <button
-              onClick={this.menuClick}
-              className="f3 b--transparent bg-transparent pointer mid-gray">
-              <FiAlignLeft />
+          className={["navbar__menu bg-near-white",
+            this.state.menuActive ? "navbar__menu__active" : null].join(' ')}>
+            <button onClick={this.menuClick}
+              className="f2 pv2 ph3 b--transparent bg-transparent pointer mid-gray absolute top-0 left-0">
+              &times;
             </button>
+            <div className="h-100 w-100 flex flex-column justify-center">
+              <div className="self-center">
+                <Link to="/" className="mid-gray no-underline">Home</Link>
+              </div>
+            </div>
           </div>
-          <div>
-            <Link
-              to="/"
-              className="f3 tracked-tight mid-gray no-underline">
-              madelyn eriksen
-            </Link>
+          <div
+            className="w-100 pa2 flex justify-between items-center">
+            <div className="w1">
+              <button
+                onClick={this.menuClick}
+                className="f3 b--transparent bg-transparent pointer mid-gray">
+                <FiAlignLeft />
+              </button>
+            </div>
+            <div>
+              <Link
+                to="/"
+                className="f3 tracked-tight mid-gray no-underline">
+                madelyn eriksen
+              </Link>
+            </div>
+            <div className="w1" />
           </div>
-          <div className="w1" />
         </div>
-      </div>
     )
   };
 };
