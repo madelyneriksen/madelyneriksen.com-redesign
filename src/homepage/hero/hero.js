c36dbcf7a57731796ba40fa70828045ef51a5f36
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-11 12:23:49 -0800

    Centered hero subtext.

--- src/homepage/hero/hero.js
+++ src/homepage/hero/hero.js
@@ -6,6 +6,6 @@ export default () => (
     <div className="ba w-75 mw6 tracked-tight pa2">
       <h1 className="tc fw3">I'm Madelyn Eriksen.</h1>
     </div>
-    <div><p className="f4 i serif">I create awesome web applications.</p></div>
+    <div><p className="f4 i serif tc">I create awesome web applications.</p></div>
   </div>
 )
