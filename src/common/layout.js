ebee1c89fc25c916bf65c3990f15919441f2225f
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-10 19:07:39 -0800

    Added some baseline styles.

--- src/common/layout.js
+++ src/common/layout.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import 'tachyons';
 
 
 export default ({ children }) => (
   <div>
+    <Helmet>
+      <body className="mid-gray bg-near-white" />
+    </Helmet>
     {children}
   </div>
 )
