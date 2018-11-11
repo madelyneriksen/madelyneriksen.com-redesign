0e97bddc1188b0c3f145404ce4a0cec9d3b8d89b
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-11 11:41:34 -0800

    Added hero unit for homepage.

---
+++ src/homepage/hero/hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default () => (
+  <div className="flex vh-75 bg-light-red flex-column justify-center items-center w-100 near-white">
+    <div className="ba w-75 mw6 tracked-tight pa2">
+      <h1 className="tc">I'm Madelyn Eriksen.</h1>
+    </div>
+    <div><p className="f4 i">I create awesome web applications.</p></div>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../common/layout.js";
 import Grid from "../homepage/grid/grid.js";
+import Hero from "../homepage/hero/hero.js";
 
 export default () => (
   <Layout>
+    <Hero />
     <Grid />
   </Layout>
 )
