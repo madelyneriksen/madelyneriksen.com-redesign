4ca2d3a545ce24e842ba14c3f979f92020bfcf59
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-11 11:49:51 -0800

    Adjusted typography on hero unit.

--- src/homepage/hero/hero.js
+++ src/homepage/hero/hero.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default () => (
   <div className="flex vh-75 bg-light-red flex-column justify-center items-center w-100 near-white">
     <div className="ba w-75 mw6 tracked-tight pa2">
-      <h1 className="tc">I'm Madelyn Eriksen.</h1>
+      <h1 className="tc fw3">I'm Madelyn Eriksen.</h1>
     </div>
-    <div><p className="f4 i">I create awesome web applications.</p></div>
+    <div><p className="f4 i serif">I create awesome web applications.</p></div>
   </div>
 )
