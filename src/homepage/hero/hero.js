62715cbad9d9d790c35385776ef874e36b233d43
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-13 06:02:46 -0800

    Updated hero unit layouts and animations 😎

--- src/homepage/hero/hero.js
+++ src/homepage/hero/hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import AnimatedText from './animated-text.js';
 
 
@@ -11,19 +12,27 @@ export default () => (
         <p className="sans-serif tracked">SOFTWARE DEVELOPER</p>
       </div>
       <div className="pv4" />
-      <div className="f1 f-subheadline-ns tracked-tight">
+      <div className="f1 f-subheadline-ns tracked-tight b">
         <AnimatedText text="MADELYN" />
         <AnimatedText text="ERIKSEN" />
       </div>
       <div className="pv4" />
       <div>
-        <p className="b sans-serif tracked">BACK END \</p>
+        <p className="b sans-serif">BACK END \</p>
         <p><span className="i">FRONT END &nbsp; </span><span className="sans-serif b">\</span></p>
-        <p className="b sans-serif tracked">OPERATIONS.</p>
+        <p className="b sans-serif">OPS.</p>
       </div>
     </div>
-    <div>
-      <p className="f4 i tc">Let's make something awesome.</p>
+    <div className="db-ns dn w-100 mw6 tr">
+      <div class="pv4">
+        <Link className="f5 green hover-light-green" to="/about">CONTACT</Link>
+      </div>
+      <div class="pv4">
+        <Link className="f5 green hover-light-green" to="/about">ABOUT</Link>
+      </div>
+      <div class="pv4">
+        <Link className="f5 green hover-light-green" to="/about">GITHUB</Link>
+      </div>
     </div>
   </div>
 )
