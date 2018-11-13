8eaa4801707d5de27575e6dc96cfb93b7f6283bf
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-13 06:03:26 -0800

    Adjusted spacing of about section and changed color.

--- src/homepage/about/about.js
+++ src/homepage/about/about.js
@@ -22,15 +22,15 @@ export default () => (
       }
     `}
     render={data => data.allAboutJson.edges.map(({node}) => (
-      <div>
-        <div className="w-100 pv5 ph2 flex flex-wrap items-center justify-center">
+      <div className="bg-light-gray">
+        <div className="w-100 pv3 ph2 flex flex-wrap items-center justify-center">
           <div className="w-100 mw6"><h1>{node.header}</h1></div>
           <div className="w-100 mw6">
             <p className="i">{node.body}</p>
             <Link className="green b hover-light-green" to="/about">About Me &raquo;</Link>
           </div>
         </div>
-        <div className="w-100 mw8 center pb2 ph2 flex flex-wrap items-center justify-center">
+        <div className="w-100 mw8 center pb3 ph2 flex flex-wrap items-center justify-center">
           {node.icons.map(icon => (
             <div className="w4 tc">
               <ScrollAnimation
