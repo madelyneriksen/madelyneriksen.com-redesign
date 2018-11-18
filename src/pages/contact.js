4f373685d1936aa9637cd229213aadf48f87b783
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-18 07:36:37 -0800

    Added contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../common/layout.js';
+
+export default () => (
+  <Layout>
+    <div className="pt5">
+    <form action="https://www.briskforms.com/go/05fe3e0bd88ddc64d0c5e7794820268c" method="POST">
+      <input type="text" name="testing" />
+      <input type="submit" value="submit" />
+    </form>
+  </div>
+  </Layout>
+)
