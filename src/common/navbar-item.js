076c628395014017b6c80faa707f6c69d32f1a02
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-17 19:26:33 -0800

    Moved to generic link.

--- src/common/navbar-item.js
+++ src/common/navbar-item.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import classNames from './functions/class-names.js';
-import { Link } from 'gatsby';
-import { Location } from '@reach/router';
+import Link from './link.js';
 
 
 export default (props) => (
-  <Location>
-    {({ location }) => (
       <div className="pa3">
-        <Link
-          to={props.to}
-          className={classNames(
-            "underline f4",
-            location.pathname === props.to ? "mid-gray no-underline" : "green hover-light-green"
-          )}>
-          {props.children}
-        </Link>
+        <Link to={props.to}>{props.children}</Link>
       </div>
-    )}
-  </Location>
 )
