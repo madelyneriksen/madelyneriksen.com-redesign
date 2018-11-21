c00fe634dba70cff1566bde21b797f6cd9ff38f9
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-21 06:41:52 -0800

    Combined grid images into one component.

---
+++ src/homepage/hero/grid/grid-images.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import Sunset from '../../../../content/about/img/sunset.jpeg';
+
+
+const ImageSlice = () => (
+  <div className="hero__image-slice" style={{backgroundImage: `url(${Sunset})`}} />
+)
+
+
+const Image = () => (
+  <div className="hero__image bg-blue" style={{backgroundImage: `url(${Sunset})`}} />
+)
+
+export default () => (
+  <Fragment>
+    <ImageSlice />
+    <Image />
+  </Fragment>
+)
--- src/homepage/hero/grid/grid.js
+++ src/homepage/hero/grid/grid.js
@@ -2,7 +2,7 @@ import React from 'react';
 import GridContainer from './grid-container.js';
 import GridJumbotron  from './grid-jumbotron.js';
 import GridNavigation from './grid-navigation.js';
-import GridImage from './grid-image.js';
+import GridImages from './grid-images.js';
 import GridSideJumbotron from './grid-side-jumbotron.js';
 import GridDescription from './grid-description.js';
 import GridSpacers from './grid-spacers.js';
@@ -11,7 +11,7 @@ import GridSpacers from './grid-spacers.js';
 export default () => (
   <GridContainer>
     <GridNavigation />
-    <GridImage />
+    <GridImages />
     <GridJumbotron />
     <GridSideJumbotron />
     <GridDescription />
