05e914b6495572e8b5a513fb521e64d01afb0803
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-22 05:44:42 -0800

    Condensed the jumbotron parts into one file.

--- src/homepage/hero/grid/grid-jumbotron.js
+++ src/homepage/hero/grid/grid-jumbotron.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Link from '../../../common/link.js';
 
 
-export default () => (
+const Jumbotron = () => (
   <div className="hero__jumbotron bg-near-white flex items-center pa3">
     <div>
       <h1 className="f1-ns mid-gray">hey! I'm Maddie.</h1>
       <span className="sans-serif mid-gray lh-copy">I'm a software developer, creating web applications and building data pipelines.</span>
     </div>
   </div>
+)
+
+const SideJumbotron = () => (
+  <div className="hero__side-jumbotron flex flex-column-l items-center items-end-l justify-around pr5-l">
+    <Link to="/about">ABOUT ME</Link>
+    <Link to="https://github.com/madelyneriksen/">PROJECTS</Link>
+    <Link to="/contact">CONTACT</Link>
+  </div>
+)
+
+const Description = () => (
+  <div class="hero__desc bg-near-white pa3 flex items-center">
+    <div>
+      <p class="mid-gray b">Currently I live in LA.</p>
+      <p class="fw1 mid-gray lh-copy">I work on data collection tools with Python and React.</p>
+    </div>
+  </div>
+)
+
+export default () => (
+  <Fragment>
+    <Jumbotron />
+    <SideJumbotron />
+    <Description />
+  </Fragment>
 )
--- src/homepage/hero/grid/grid.js
+++ src/homepage/hero/grid/grid.js
@@ -3,8 +3,6 @@ import GridContainer from './grid-container.js';
 import GridJumbotron  from './grid-jumbotron.js';
 import GridNavigation from './grid-navigation.js';
 import GridImages from './grid-images.js';
-import GridSideJumbotron from './grid-side-jumbotron.js';
-import GridDescription from './grid-description.js';
 import GridSpacers from './grid-spacers.js';
 
 
@@ -13,8 +11,6 @@ export default () => (
     <GridNavigation />
     <GridImages />
     <GridJumbotron />
-    <GridSideJumbotron />
-    <GridDescription />
     <GridSpacers />
   </GridContainer>
 )
