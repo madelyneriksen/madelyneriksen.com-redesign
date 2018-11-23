4cdfea432bd7d590406809e8ceba24382c1421be
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-23 07:32:19 -0800

    Removed old content grid.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Layout from "../common/layout.js";
-import Grid from "../homepage/grid/grid.js";
 import Hero from "../homepage/hero/hero.js";
 import Bio from "../homepage/bio/bio.js";
 
