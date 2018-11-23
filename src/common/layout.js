c8c081163f8d8c8a6b8470f4cb1c49a79f0095df
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-23 07:31:34 -0800

    Removed old navbar.

--- src/common/layout.js
+++ src/common/layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import Navbar from './navbar.js';
 import 'tachyons';
 import './styles/custom.tachyons.css';
 
--- src/common/navbar-item.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Link from './link.js';
-
-
-export default (props) => (
-      <div className="pa3">
-        <Link to={props.to}>{props.children}</Link>
-      </div>
-)
