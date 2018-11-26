3bb7f2ce0ca043a7074ff9dd4bc5c3346d6becbe
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-11-26 06:37:55 -0800

    Added recent posts to homepage

---
+++ src/homepage/recent-posts/recent-posts.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import '../styles/generic-grid.css';
+import { StaticQuery, graphql, Link } from 'gatsby';
+
+
+export default () => (
+  <React.Fragment>
+    <div className="generic-grid">
+      <div className="flex items-center justify-end" style={{gridArea: "bottom"}}>
+        <h4><Link to="/blog/" className="no-underline mid-gray">See all posts &raquo;</Link></h4>
+      </div>
+      <StaticQuery
+        query={graphql`
+          query {
+            allMarkdownRemark(
+              filter: {frontmatter: {type: {eq: "post"}}},
+              limit: 3,
+              sort: {fields: [frontmatter___date], order: DESC}) {
+              edges {
+                node {
+                  frontmatter {
+                    title
+                    path
+                    excerpt
+                    featuredImage {
+                      childImageSharp {
+                        fluid(maxHeight: 800) {
+                          src
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }  
+        `}
+        render={data => data.allMarkdownRemark.edges.map(({node}, index) => (
+          <div style={{gridArea: "side-"+(index+1)}}>
+            <Link to={node.frontmatter.path} className="grow db">
+              <img src={node.frontmatter.featuredImage.childImageSharp.fluid.src} />
+            </Link>
+            <div className="ph2">
+              <h2>
+                <Link
+                  to={node.frontmatter.path}
+                  className="mid-gray no-underline">{node.frontmatter.title}</Link>
+              </h2>
+              <p className="f4 lh-copy">{node.frontmatter.excerpt}</p>
+            </div>
+          </div>
+        ))}
+      />
+    </div>
+  </React.Fragment>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Layout from "../common/layout.js";
 import Hero from "../homepage/hero/hero.js";
+import RecentPosts from "../homepage/recent-posts/recent-posts.js";
 import Bio from "../homepage/bio/bio.js";
 
 export default () => (
   <Layout>
     <Hero />
+    <RecentPosts />
     <Bio />
   </Layout>
 )
