e852cf7a95ae0586a0b461c3bd637c7293458544
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-01 08:36:35 -0800

    Changed bio section futher.
    Currently I am thinking of replacing the 'About' page with this bio
    section. I could also move the contact form to the main page.
    
    However, I do want to incorporate more whitespace into the design. I
    might have to make some css changes to the grid for that.

--- src/homepage/bio/bio-copy.js
+++ src/homepage/bio/bio-copy.js
@@ -3,7 +3,10 @@ import classNames from '../../common/functions/class-names.js';
 import { StaticQuery, graphql } from 'gatsby';
 import ScrollAnimation from 'react-animate-on-scroll';
 import Me from '../../../content/about/img/me.jpeg';
+import Juniper from '../../../content/about/img/juniper.png';
+import Pueblo from '../../../content/about/img/pueblo.jpg';
 import 'animate.css/animate.min.css';
+import '../../styles/font-mfizz.css';
 
 
 const Panel = props => (
@@ -28,8 +31,18 @@ export default () => (
     `}
     render={data => (
       <React.Fragment>
-        <Panel justify="justify-end" gridArea="side-1">
-          <ScrollAnimation animateIn="fadeInDown" animateOnce={true}>
+        <div style={{gridArea: "side-1"}}>
+          <ScrollAnimation animateIn="fadeInUp" animateOnce={true} className="h-100">
+            <img src={Pueblo} className="h-100"/>
+          </ScrollAnimation>
+        </div>
+        <div style={{gridArea: "side-3"}} className="flex flex-column justify-around items-center">
+          <ScrollAnimation className="db icon-reactjs f-headline" animateIn="fadeInUp" animateOnce={true} />
+          <ScrollAnimation className="db icon-python f-headline" animateIn="fadeInUp" animateOnce={true} />
+          <ScrollAnimation className="db icon-nodejs f-headline" animateIn="fadeInUp" animateOnce={true} />
+        </div>
+        <Panel justify="justify-center" gridArea="side-2">
+          <ScrollAnimation animateIn="fadeInUp" animateOnce={true} className="pa2">
             <img src={Me} className="db mw5 br-100 center" />
             <h1 className="tc">{data.side1.frontmatter.header}</h1>
             <div className="lh-copy f4" dangerouslySetInnerHTML={{__html: data.side1.html}} />
