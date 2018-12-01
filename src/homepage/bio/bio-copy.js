998d94e565e4e05925f0ac76be6743b2dfec4a0f
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-01 09:40:37 -0800

    Increased whitespace on desktop design and changed media queries.

--- src/homepage/bio/bio-copy.js
+++ src/homepage/bio/bio-copy.js
@@ -3,7 +3,6 @@ import classNames from '../../common/functions/class-names.js';
 import { StaticQuery, graphql } from 'gatsby';
 import ScrollAnimation from 'react-animate-on-scroll';
 import Me from '../../../content/about/img/me.jpeg';
-import Juniper from '../../../content/about/img/juniper.png';
 import Pueblo from '../../../content/about/img/pueblo.jpg';
 import 'animate.css/animate.min.css';
 import '../../styles/font-mfizz.css';
@@ -36,12 +35,7 @@ export default () => (
             <img src={Pueblo} className="h-100"/>
           </ScrollAnimation>
         </div>
-        <div style={{gridArea: "side-3"}} className="flex flex-column justify-around items-center">
-          <ScrollAnimation className="db icon-reactjs f-headline" animateIn="fadeInUp" animateOnce={true} />
-          <ScrollAnimation className="db icon-python f-headline" animateIn="fadeInUp" animateOnce={true} />
-          <ScrollAnimation className="db icon-nodejs f-headline" animateIn="fadeInUp" animateOnce={true} />
-        </div>
-        <Panel justify="justify-center" gridArea="side-2">
+        <Panel justify="justify-around" gridArea="side-3">
           <ScrollAnimation animateIn="fadeInUp" animateOnce={true} className="pa2">
             <img src={Me} className="db mw5 br-100 center" />
             <h1 className="tc">{data.side1.frontmatter.header}</h1>
