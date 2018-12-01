517c19e494f583bd3ac62f90c429954388e16eae
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-01 13:09:43 -0800

    Updated contact form layout and styling.

--- src/homepage/contact/contact.js
+++ src/homepage/contact/contact.js
@@ -3,5 +3,11 @@ import Form from './form.js';
 
 
 export default () => (
-  <div id="contact" />
+  <div className="generic-grid min-vh-100-l" id="contact">
+    <div style={{gridArea: "side-3"}}>
+      <Form />
+    </div>
+    <div style={{gridArea: "side-1"}}>
+    </div>
+  </div>
 )
--- src/homepage/contact/form.js
+++ src/homepage/contact/form.js
@@ -2,27 +2,30 @@ import React from 'react';
 import './styles/form-grid.css';
 
 
-const formClasses = "w-100 bg-transparent br-0 bl-0 bt-0 bb b--mid-gray pa2";
+const formClasses = "w-100 bg-transparent br-0 bl-0 bt-0 bb b--mid-gray pa2 mid-gray contact__form-item";
 
 
 export default () => (
-  <form
-    action="https://www.briskforms.com/go/05fe3e0bd88ddc64d0c5e7794820268c"
-    method="POST"
-    className="h-100 flex items-center justify-center">
-      <div className="contact__form-grid w-100 pa2 mw6">
+  <React.Fragment>
+    <h1 className="tc">Contact Me</h1>
+    <form
+      action="https://www.briskforms.com/go/05fe3e0bd88ddc64d0c5e7794820268c"
+      method="POST"
+      className="h-100 flex items-center justify-center">
+      <div className="contact__form-grid w-100 pa2">
         <input type="text" name="name" placeholder="Name"
           className={formClasses} required />
         <input type="text" name="email" placeholder="Email Address"
           className={formClasses} required />
         <input
           type="text" name="subject" placeholder="Subject"
-          className={formClasses + " contact__double-wide"} required />
+          className={formClasses} required />
         <textarea name="message" placeholder="Questions, comments, concerns..." style={{resize: "none"}}
-          className={formClasses + " contact__double-wide"} rows="5" required />
-        <div className="contact__double-wide tc">
-          <input type="submit" value="Submit" className="w-100 mw5 ba b--black pa2"/>
+          className={formClasses} rows="5" required />
+        <div className="tc">
+          <input type="submit" value="SUBMIT" className="w-100 mw5 bn bg-washed-red pa3 mid-gray pointer grow b"/>
         </div>
       </div>
-  </form>
+    </form>
+  </React.Fragment>
 )
