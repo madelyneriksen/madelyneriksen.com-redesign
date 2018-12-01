667bed0fa067b43bc568f712b50dfbeccf0f7389
Madelyn Eriksen <madelyn.eriksen@gmail.com>
2018-12-01 09:50:04 -0800

    Removed contact page for one page contact.

--- src/pages/contact.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Grid from '../contact/grid/grid.js';
-import Layout from '../common/layout.js';
-
-export default () => (
-  <Layout>
-    <Grid />
-  </Layout>
-)
